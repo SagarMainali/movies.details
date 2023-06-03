b060e974e2e8f53bcf3474df132c761fde1544a7
Sagar Mainali <sagarmainali78@gmail.com>
2023-06-03 16:58:02 +0545

    added error page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+function PageNotFound() {
+     return (
+          <div className="flex flex-col items-center justify-center h-screen w-screen text-white">
+               <h2>Invalid URL</h2>
+               <p>The page you are requesting could not be found.🤷🏻‍♂️</p>
+          </div>
+     )
+}
+
+export default PageNotFound
