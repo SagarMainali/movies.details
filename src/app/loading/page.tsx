9f08c5092fbd7ef84e1013eb99abeda62abed9b9
Sagar Mainali <sagarmainali78@gmail.com>
2023-06-03 16:41:50 +0545

    added loading file

---
+++ src/app/loading/page.tsx
@@ -0,0 +1,20 @@
+import './loading.css'
+
+function loading() {
+     return (
+          <div className='loading-container'>
+               <div className="lds-roller">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+               </div>
+          </div>
+     )
+}
+
+export default loading
