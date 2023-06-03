d0ccda4712b48004e153f7ed4bef68019379ba8e
Sagar Mainali <sagarmainali78@gmail.com>
2023-06-03 17:27:26 +0545

    error page final

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,8 @@
 function PageNotFound() {
      return (
-          <div className="flex flex-col items-center justify-center h-screen w-screen text-white">
+          <div className="flex items-center justify-center gap-2 h-screen w-screen text-white">
                <h2>Invalid URL</h2>
+               <span>|</span>
                <p>The page you are requesting could not be found.🤷🏻‍♂️</p>
           </div>
      )
