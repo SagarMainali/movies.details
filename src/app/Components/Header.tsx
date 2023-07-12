44a402ea043daaace9fe26f77e506ff5bd5806d2
Sagar Mainali <sagarmainali78@gmail.com>
2023-07-12 17:16:06 +0545

    meta update, favicon added and theme updated

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -5,12 +5,12 @@ function Header() {
      return (
           <div className="flex justify-between items-center py-4">
                <Link href={'./'}>
-                    <Image src={'./tmdb-logo.svg'} alt="tmbd-logo" width={140} height={100} />
+                    <Image src={'tmdb-logo.svg'} alt="tmbd-logo" width={140} height={100} />
                </Link>
                <ul className="flex gap-4 items-center">
                     <Link href={'./movies'}>Movies</Link>
                     <Link href={'./tvshows'}>Tv Shows</Link>
-                    <input type="text" className="bg-slate-200 rounded-md outline-0 py-1 px-2 sm:w-[10em] w-[8rem]" />
+                    <input type="text" className="bg-slate-200 rounded-md caret-slate-400 outline-0 px-2 sm:w-[10em] w-[8rem]" />
                </ul>
           </div>
      )
